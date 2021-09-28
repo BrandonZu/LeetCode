8c8493fd0ee7170203d3125f77415106aed7da9f
brandonzu <brandon_zu@qq.com>
2021-09-27 23:23:43 -0700

    09/27/2021 2 Prob Solved(1H+1M)

--- C++/3rd_072021-112021/215_KthLargestElementinanArray.cpp
+++ C++/3rd_072021-112021/215_KthLargestElementinanArray.cpp
@@ -2,4 +2,44 @@
 // Created by BrandonZu on 9/26/21.
 //
 
+#include "common.h"
+
+// Runtime 4ms(>98%) | Memory Usage 10MB(>59%)
+class Solution {
+public:
+    int partition(vector<int>& nums, int left, int right) {
+        int l = left, r = right;
+        int pivot = nums[(left + right) / 2];
+        swap(nums[left], nums[(left + right) / 2]);
+        while(l < r) {
+            while(l < r && nums[r] >= pivot)
+                r--;
+            nums[l] = nums[r];
+            while(l < r && nums[l] < pivot)
+                l++;
+            nums[r] = nums[l];
+        }
+        nums[l] = pivot;
+        return l;
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while(l < r) {
+            int pos = partition(nums, l, r);
+            if(r - pos + 1 == k) {
+                return nums[pos];
+            }
+            else if(r - pos + 1 > k) {
+                l = pos + 1;
+            }
+            else {
+                k = k - (r - pos + 1);
+                r = pos - 1;
+            }
+        }
+        return nums[l];
+    }
+};
 
--- C++/3rd_072021-112021/224_BasicCalculator.cpp
+++ C++/3rd_072021-112021/224_BasicCalculator.cpp
@@ -2,4 +2,75 @@
 // Created by BrandonZu on 9/27/21.
 //
 
+#include "common.h"
 
+// Runtime 20ms(>29%) | Memory Usage 9.2MB(>25%)
+class Solution {
+public:
+    void cal(stack<int>& nums, stack<char>& oper) {
+        char op = oper.top();
+        oper.pop();
+        int num2 = nums.top();
+        nums.pop();
+        int num1 = nums.top();
+        nums.pop();
+        if(op == '+') {
+            nums.push(num1 + num2);
+        }
+        else if(op == '-') {
+            nums.push(num1 - num2);
+        }
+    }
+
+    int calculate(string s) {
+        // Remove Spaces
+        int j = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == ' ')
+                continue;
+            else
+                s[j++] = s[i];
+        }
+        stack<int> nums;
+        stack<char> oper;
+        for(int pos = 0; pos < j; pos++) {
+            if(s[pos] == '(') {
+                oper.push(s[pos]);
+            }
+            else if(s[pos] == '-' || s[pos] == '+') {
+                // IMPORTANT
+                if (pos == 0 || s[pos - 1] == '(' || s[pos - 1] == '+' || s[pos - 1] == '-')
+                    nums.push(0);
+                while(!oper.empty() && oper.top() != '(')
+                    cal(nums, oper);
+                oper.push(s[pos]);
+            }
+            else if(s[pos] == ')') {
+                while(!oper.empty() && oper.top() != '(')
+                    cal(nums, oper);
+                oper.pop();
+            }
+            else if(isdigit(s[pos])) {
+                int num = 0;
+                while(pos < s.size() && isdigit(s[pos])) {
+                    num = num * 10 + (s[pos] - '0');
+                    pos++;
+                }
+                nums.push(num);
+                pos--;
+            }
+        }
+
+        while(!oper.empty()) {
+            cal(nums, oper);
+        }
+        return nums.top();
+    }
+};
+
+int main() {
+    Solution s;
+    string tmp = "(1+(4+5+2)-3)+(6+8)";
+    s.calculate(tmp);
+
+}
