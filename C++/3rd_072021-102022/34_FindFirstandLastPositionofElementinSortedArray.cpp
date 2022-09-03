08276b1ebf06f2162b03410eb113cb3df56a3fa9
brandonzu <brandonzu.coder@gmail.com>
2022-09-03 09:45:43 -0400

    9/1/22 1M

--- C++/3rd_072021-102022/34_FindFirstandLastPositionofElementinSortedArray.cpp
+++ C++/3rd_072021-102022/34_FindFirstandLastPositionofElementinSortedArray.cpp
@@ -35,3 +35,40 @@ public:
         return result;
     }
 };
+
+// R .25 | M .84
+class Solution2 {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(nums.empty()) return {-1, -1};
+        int left = -1, right = -1;
+        int l = 0, r = nums.size() - 1;
+        // left most
+        while(l < r) {
+            int mid = (l + r) / 2;
+            if(nums[mid] >= target) {
+                r = mid;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        left = l;
+        if(nums[left] != target) {
+            return {-1, -1};
+        }
+        // right most
+        l = 0, r = nums.size() - 1;
+        while(l < r) {
+            int mid = (l + r + 1) / 2;
+            if(nums[mid] <= target) {
+                l = mid;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        right = l;
+        return {left, right};
+    }
+};
--- C++/Practice/playground.cpp
+++ C++/Practice/playground.cpp
@@ -2,11 +2,8 @@
 
 int main() {
     unordered_map<int, int> myMap = {{1, 3}, {2, 4}, {3, 6}, {6, 12}, {19, 99}};
-    for(auto& [k, v] : myMap) {
-        while(true)  {
-            cout << k << ":" << v << endl;
-        }
-
+    for(const auto& [k, v] : myMap) {
+        cout << k << ":" << v << endl;
     }
     return 0;
 }
